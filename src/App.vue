ce4408412ca2cab6a8753892cfdce617742fef3d
Giulia Carmen Dai <giuliadai1508@gmail.com>
2023-03-29 16:34:41 +0200

    creazione header component

---
+++ src/App.vue
@@ -0,0 +1,43 @@
+<script>
+import AppHeader from './components/AppHeader.vue';
+import axios from 'axios';
+import { store } from './store.js';
+
+export default {
+  components: {
+    AppHeader,
+  },
+  data() {
+    return {
+      store
+    }
+  },
+  methods: {
+    getCards() {
+      axios.get('https://db.ygoprodeck.com/api/v7/cardinfo.php?name=Dark Magician')
+        .then(results => {
+          this.store.cardList = results.data.results;
+        });
+    }
+  },
+  created() {
+    this.getCards();
+  }
+}
+</script>
+
+<template>
+  <header>
+    <AppHeader title="Yu-Gi-Oh Api"></AppHeader>
+  </header>
+
+  <main>
+
+  </main>
+</template>
+
+<style lang="scss" scoped>
+main {
+  background-color: #D48F38;
+}
+</style>
---
+++ src/components/AppHeader.vue
@@ -0,0 +1,31 @@
+<script>
+export default {
+    name: 'AppHeader',
+    props: {
+        title: String
+    }
+}
+</script>
+
+<template>
+    <div class="wrapper">
+        <img src="../assets/logo.jpg" alt="logo yu-gi-oh">
+        <h1>{{ title }}</h1>
+    </div>
+</template>
+
+<style lang="scss" scoped>
+h1 {
+    background-color: white;
+    color: black;
+    font-weight: 200;
+    font-size: 50px;
+    display: inline-block;
+    vertical-align: middle;
+}
+
+img {
+    width: 8%;
+    vertical-align: middle;
+}
+</style>
